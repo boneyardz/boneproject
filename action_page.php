7d322452563fc094807ab5ec6ccdb57de93e619e
bonesjohn <johnargles1@gmail.com>
2021-04-09 17:08:37 +0100

    php update
    Signed-off-by: bonesjohn <johnargles1@gmail.com>

---
+++ action_page.php
@@ -0,0 +1,17 @@
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="mystyle.css">
+  </head>
+  <body>
+    <main><!-- this is our container -->
+      <header>
+      </header>
+      <p>
+        This is the landing page!
+      </p>
+      <?php
+        echo("<h2>Hello world</h2>");
+      ?>
+    </main>
+  </body>
+</html>
