155f7e05394be587da3297142b66fa9f2d675387
bonesjohn <johnargles1@gmail.com>
2021-04-15 17:18:24 +0100

    added php comments to index.php.
    Signed-off-by: bonesjohn <johnargles1@gmail.com>

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
-<?php include("includes/page_top.php"); ?>
+<?php
+/**
+ * index.php is the entry page for our website
+  * @author johnargles
+   * @version 15/04/2021
+    */
+  /* bring in the starting html for our webpage */
+     include("includes/page_top.php"); ?>
       <form action="action_page.php">
         <!-- label for="key">Room Key:</label -->
         <input type="text" id="key" name="key" placeholder="Room Key">
@@ -6,4 +13,6 @@
         <input type="text" id="name" name="name" placeholder="Your Name">
         <input type="submit" value="Enter">
       </form>
-<?php include("includes/page_bottom.php"); ?>
+<?php
+  // bring in the ending html for our webpage
+   include("includes/page_bottom.php"); ?>
