123342d30fa94e92b9256226f406b1361fc0cb26
bonesjohn <johnargles1@gmail.com>
2021-04-27 11:27:04 +0100

    Created Word game.php

---
+++ BlankWord.php
@@ -0,0 +1,8 @@
+<?php include("includes/page_top.php"); ?>
+<h3>BlankWord</h3>
+<form action="fred.php">
+  <label 4="answer">word</label>
+  <input type="text" id="answer" name="answer" placeholder="Your Answer">
+  <input type="submit" value="Enter">
+</form>
+<?php include("includes/page_bottom.php"); ?>
