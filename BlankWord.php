c86e6fe8f45d046dec0d9da883362213eaadd02c
bonesjohn <johnargles1@gmail.com>
2021-04-27 11:46:20 +0100

    open words file

--- BlankWord.php
+++ BlankWord.php
@@ -1,7 +1,11 @@
-<?php include("includes/page_top.php"); ?>
+<?php include("includes/page_top.php");
+  $word="";
+  $list[]="";
+  filehandle=fopen("resources/blank_word.txt","r");
+?>
 <h3>BlankWord</h3>
 <form action="fred.php">
-  <label 4="answer">word</label>
+  <label for="answer"><?php echo($word); ?></label>
   <input type="text" id="answer" name="answer" placeholder="Your Answer">
   <input type="submit" value="Enter">
 </form>
