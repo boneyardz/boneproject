2653103e92d9f28126ffac02ae69693682579c2a
bonesjohn777 <johnargles1@gmail.com>
2021-05-12 23:49:11 +0100

    updates

--- BlankWord.php
+++ BlankWord.php
@@ -2,17 +2,28 @@
   error_reporting(E_ALL);
   include("includes/page_top.php");
   $word="";
-  $list[]="";
-  $filehandle=fopen("resources/blank_word.txt","r");
-  $filehandle=fopen("resources/word_blank.txt","r");
-  while($row=fgetcsv($filehandle))
-  {
-    //$list[]=$row[0];
-  }
+  //GET THE WORDS FROM BLANK_WORD
+  $filehandle=fopen("resources/blank_word.txt","r"); //175 WORDS
+  while($row=fgetcsv($filehandle)) $BlankWord[]=$row[0];
+  fclose($filehandle);
+  //GET THE WORDS FROM WORD_BLANK
+  $filehandle=fopen("resources/word_blank.txt","r"); //323 WORDS
+  while($row=fgetcsv($filehandle)) $WordBlank[]=$row[0];
   fclose($filehandle);
   //print_r($list);
   //echo(count($list));
   //echo(rand(1,count($list)-1));
+  $choice=random_int(1,2);
+  switch($choice){
+    case1:
+      $word=$BlankWord[random_int(1,count($BlankWord)-1)];
+      break;
+    case2:
+      $word=$WordBlank[random_int(1,count($WordBlank)-1)];
+      break;
+      default:
+        $word="oops";
+	}
   $word=$list[random_int(1,count($list)-1)];
 ?>
 <h3>BlankWord</h3>
